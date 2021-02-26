bd0d878503a533c10bee83044863f87cde54ed6e
Colin <colin@umn.edu>
2021-02-25 20:55:47 -0600

    Styling corresponding items in lists similarly

--- docs/beowulf/listgrouper.js
+++ docs/beowulf/listgrouper.js
@@ -1,3 +1,5 @@
+'use strict'
+
 console.log('In listgrouper.js')
 
 //export
@@ -6,9 +8,49 @@ class ListGrouper extends HTMLElement {
     super()
     console.log('In constructor')
   }
+
   connectedCallback() {
     console.log('connected')
     //console.log(this.innerHTML)
+
+    const idxMap = new WeakMap()
+
+    const lists = this.querySelectorAll('ul, ol')
+
+    lists.forEach(list => {
+        const items = list.children
+        for (let i=0; i < items.length; ++i ) {
+          idxMap.set(items[i], i)
+          console.log(i)
+        }
+      })
+
+    this.addEventListener('mouseover', ev => {
+        if (event.target.tagName === 'LI') {
+          const idx = idxMap.get(event.target)
+          lists.forEach(list => {
+            const item = list.children[idx]
+            // Any difference in perceived responsiveness
+            // using class vs direct styling?  Not that
+            // I can see.
+            item.style.color = 'blue'
+            item.classList.add('idxhover')
+          })
+          //event.target.style.color = 'blue'
+          //console.log(event.target.value)
+        }
+      })
+    this.addEventListener('mouseout', ev => {
+        if (event.target.tagName === 'LI') {
+          let idx = idxMap.get(event.target)
+          lists.forEach(list => {
+            const item = list.children[idx]
+            item.style.color = 'red'
+            item.classList.remove('idxhover')
+          })
+          //event.target.style.color = 'red'
+        }
+      })
   }
 }
 
