8440d2a87a9e92386a887679c511b4000feac155
clncmbl <999424+clncmbl@users.noreply.github.com>
2025-04-21 20:11:56 -0500

    Working on layout with HTML wrapper

--- docs/brunanburh/main.js
+++ docs/brunanburh/main.js
@@ -7,11 +7,37 @@ function setImportMap() {
   imp.type = 'importmap';
   imp.textContent = JSON.stringify({
     "imports": {
-      "TextGloss": cdn + "TextGloss/TextGloss.js"
+      "TextGloss": cdn + "TextGloss/TextGloss.js",
+      "disableTextGloss": "/text-tags/TextGloss/TextGloss.js"
     }
   });
   document.currentScript.after(imp);
 }
 
 setImportMap();
+
+// Appending to head.innerHTML apparently must happen after any calls
+// to currentScript.after.  Otherwise, the currentScript.after call
+// does not work.
+document.head.innerHTML += `
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="main.css" blocking="render">
+`;
+
+async function wrapArticle() {
+  const docfrag = document.createDocumentFragment();
+  docfrag.appendChild(document.querySelector('body>article'));
+
+  const response = await fetch('wrapper.html');
+  const html = await response.text();
+  document.body.innerHTML = html;
+
+  document.getElementById('contentcontainer').appendChild(docfrag);
+  document.body.style.visibility = 'visible';
+}
+
+window.addEventListener('DOMContentLoaded', async ev => {
+  await wrapArticle();
+  import('TextGloss');
+});
 
